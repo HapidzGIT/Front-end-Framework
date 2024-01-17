1214999136f9688e81394b0b151b9f7fde5ffa44
HapidzGIT <hpsgaming212@gmail.com>
2024-01-17 10:19:25 +0700

    Frist Commit From The Folder

---
+++ src/views/Dashboard/index.jsx
@@ -0,0 +1,10 @@
+//import layout
+import LayoutDefault from "../../layouts/Default.jsx";
+
+export default function Dashboard () {
+    return (
+        <LayoutDefault>
+            <h1 style={{color: 'black'}}>Halaman Dashboard</h1>
+        </LayoutDefault>
+    )
+}
